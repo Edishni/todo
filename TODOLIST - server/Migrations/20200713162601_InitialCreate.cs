0b5dda049b9b197eabddb06cdf5663f42941e294
Edward Tchaevsky <57283717+Edishni@users.noreply.github.com>
2020-07-13 22:28:37 +0300

    3 add server
    add server to git

---
+++ TODOLIST - server/Migrations/20200713162601_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication11.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "todo",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    taskName = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    data = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_todo", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todo");
+        }
+    }
+}
---
+++ TODOLIST - server/Models/TodoDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication11.Models
+{
+    public class TodoDetails
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string taskName { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string description { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string data { get; set; }
+    }
+}
